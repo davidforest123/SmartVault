b4f0e0b6bda803955baab1c291ddbe868fba44e8
kiramoko <107166009+davidforest123@users.noreply.github.com>
2023-03-15 16:24:46 +0800

    add showChoiceDialog

--- lib/widgets/widget_alert_dialog.dart
+++ lib/widgets/widget_alert_dialog.dart
@@ -21,3 +21,26 @@ void showAlertDialog(BuildContext context, String title, String message,
     ),
   );
 }
+
+Future<dynamic> showChoiceDialog(
+    BuildContext context, String title, String message, List<String> choices) {
+  return showDialog<String>(
+    context: context,
+    builder: (ctx) => AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: List.generate(
+        choices.length,
+        (index) => ElevatedButton(
+          onPressed: () {
+            Navigator.of(ctx).pop(choices[index]);
+          },
+          child: Container(
+            padding: const EdgeInsets.all(14),
+            child: Text(choices[index]),
+          ),
+        ),
+      ),
+    ),
+  );
+}
